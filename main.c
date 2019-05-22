d12d37da1f3db1dc44e7acd38f619a69a64bf0fc
Gutha <gutha.3@asc.ohio-state.edu>
2019-05-22 17:03:28 -0400

    MAP: hw and lw bridges

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 #include "soc_cv_av/socal/hps.h"
 #include "soc_cv_av/socal/alt_gpio.h"
 
+#include "hps.h"
+#include "base_addresses.h"
+#include "quartus.h"
+
 // pointers for virtual bases
 void *h2p_lw_axi_vbase;
 void *h2p_hw_axi_vbase;
@@ -26,7 +30,7 @@ int main(int argc, char const *argv[])
     }
 
     // get the base address for LW axi bridge
-    h2p_lw_vbase = mmap(
+    h2p_lw_axi_vbase = mmap(
         NULL,
         HW_REGS_SPAN,
         (PROT_READ | PROT_WRITE),
@@ -34,14 +38,30 @@ int main(int argc, char const *argv[])
         fd,
         HW_REGS_BASE);
 
-    if (h2p_lw_vbase == MAP_FAILED)
+    if (h2p_lw_axi_vbase == MAP_FAILED)
     {
-        printf("ERROR: mmap failed... \n");
+        printf("ERROR: lw axi mmap() failed... \n");
         close(fd);
         return 1;
     }
 
     // get the base address for the HW axi bridge
+    h2p_hw_axi_vbase = mmap(
+        NULL,
+        HW_FPGA_AXI_SPAN,
+        (PROT_READ | PROT_WRITE),
+        MAP_SHARED,
+        fd,
+        ALT_AXI_FPGASLVS_OFST);
+
+    if (h2p_hw_axi_vbase == MAP_FAILED)
+    {
+        printf("ERROR: hw axi mmap() failed... \n");
+        close(fd);
+        return 1;
+    }
+
+    
 
     return 0;
 }
